1607e234ab3b982e8690b84db5a3208c3b68ab1f
Nishant Mohan <128303476+Nishant040305@users.noreply.github.com>
2023-11-04 15:49:25 +0530

    Add files via upload

---
+++ Currency/Currency_Converter/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path,include
+from Currency_Converter import views
+urlpatterns = [
+    #it will direct it to index func of views
+    path('',views.index,name = 'home')
+]
---
+++ Currency/Currency_Converter/views.py
@@ -0,0 +1,22 @@
+
+from django.http import JsonResponse
+import requests
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index2.html')
+
+def convert_currency(request):
+    amount = request.GET.get('amount')
+    from_currency = request.GET.get('from_currency')
+    to_currency = request.GET.get('to_currency')
+
+    # Make sure to replace 'your_api_key' with your actual API key
+    url = f"http://api.exchangeratesapi.io/v1/latest?access_key=aa54007b6c5e6136b3eba5cf93f4a9a9{from_currency}"
+    response = requests.get(url)
+    data = response.json()
+
+    conversion_rate = data['rates'][to_currency]
+    converted_amount = float(amount) * conversion_rate
+
+    return JsonResponse({'result': converted_amount})
