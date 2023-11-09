560a659ceab370a7583ef0a4ba8fd05ec68fc7fc
Nishant Mohan <128303476+Nishant040305@users.noreply.github.com>
2023-11-09 11:41:54 +0530

    Delete Currency directory

--- Currency/Currency_Converter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CurrencyConverterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Currency_Converter'
--- Currency/Currency_Converter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from Currency_Converter import views
-urlpatterns = [
-    #it will direct it to index func of views
-    path('',views.index,name = 'home')
-]
