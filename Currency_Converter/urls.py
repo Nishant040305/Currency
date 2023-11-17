e2cf8e635aac8861d3a98350fb187715ea0fe6c8
Mukund Bihari Gupta <142800930+mukund-gupta21@users.noreply.github.com>
2023-11-17 20:19:39 +0530

    Update urls.py

--- Currency_Converter/urls.py
+++ Currency_Converter/urls.py
@@ -4,7 +4,5 @@ from Currency_Converter import views
 urlpatterns = [
     #it will direct it to index func of views
     path('',views.index,name = 'home'),
-    path('fav',views.fav,name='fav'),
-    path('calculator/',views.calculator,name='calculator'),
-    path('news/',views.news_feed,name="News")
+
 ]
