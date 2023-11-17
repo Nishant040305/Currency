f9a843702a655e87ab73464c91958a395a4a56a3
Nishant Mohan <128303476+Nishant040305@users.noreply.github.com>
2023-11-17 20:01:57 +0530

    Update views.py

--- Currency_Converter/views.py
+++ Currency_Converter/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+import time
+import threading
 from django.http import JsonResponse
 import requests
 import feedparser
@@ -10,18 +11,35 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from .models import favPain
 from .models import History
 
-
 csv_file_path = staticfiles_storage.path('currency_name.csv')
 content = {}
 with open(csv_file_path,'r') as f:
     a =( reader(f))
     for i in a:
-        content[i[0]]=i[1]
+        content[f"""/static/flag/{i[0]}.png"""]=i[1]
+duration = 6000
+start = time.time()
+url ="https://api.exchangerate-api.com/v4/latest/USD"
+response = requests.get(url).json()
+rates = response[ "rates"]
 def index(request):
     global content
+    global response
+    global start
+    global duration
+    global rates
+    
     favpair = favPain.objects.all()
     history = History.objects.all()
-
+    try:
+        if((time.time()-start)>duration):
+            url ="https://api.exchangerate-api.com/v4/latest/USD"
+            response = requests.get(url).json()
+            start = time.time()
+            rates = response['rates']
+    except:
+        pass
+    
     if request.GET.get('fav'):
         tAmount=request.GET.get('bamount')
         fAmount=request.GET.get('oamount')
@@ -31,25 +49,25 @@ def index(request):
     
             data = favPain.objects.create(fromCode=field1_data, toCode=field2_data)
             data.save()
-        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'history':history,'favpair':favpair})
+        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'history':history,'favpair':favpair,'rates':rates})
 
     elif request.GET.get('convert'):
         try:
-            fcurrencyCode=str(request.GET['fromCode'])
+            fcurrencyCode=request.GET['fromCode']
             fAmount=request.GET['oamount']
             tcurrencyCode=request.GET['toCode']
-            url ="https://api.exchangerate-api.com/v4/latest/USD"
-            response = requests.get(url).json()
-            tAmount=response['rates'][tcurrencyCode]/response['rates'][fcurrencyCode]  * float(fAmount) 
+            tAmount=rates[tcurrencyCode]/rates[fcurrencyCode]  * float(fAmount) 
             tAmount=round(tAmount,3)
             data = History.objects.create(fcurrencyCode=fcurrencyCode,fAmount=fAmount,tcurrencyCode=tcurrencyCode,tAmount=tAmount)
             data.save()
+            
         except:
-            tAmount='0.00'
-            fcurrencyCode='Search'
-            fAmount='0.00'
-            tcurrencyCode='Search'
-        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':fcurrencyCode,'tcurrencyCode':tcurrencyCode,'history':history,'favpair':favpair})
+            tAmount=0.00
+            fcurrencyCode=request.GET['fromCode']
+            fAmount=0.00
+            tcurrencyCode=request.GET['toCode']
+
+        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':fcurrencyCode,'tcurrencyCode':tcurrencyCode,'history':history,'favpair':favpair,'rates':rates})
 
     elif request.GET.get('calcwindow'):
         
@@ -58,13 +76,10 @@ def index(request):
         fAmount=request.GET.get('oamount')
         field1_data = request.GET.get('fromCode')
         field2_data = request.GET.get('toCode')
-        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'favpair':favpair,'history':history})
+        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'favpair':favpair,'rates':rates,'history':history})
     else:
         tAmount='0.00'
         fcurrencyCode='Search'
-        fAmount='0.00'
+        fAmount=''
         tcurrencyCode='Search'
-        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':fcurrencyCode,'tcurrencyCode':tcurrencyCode,'history':history,'favpair':favpair})
-
-       
-    
+        return render(request,'index.html',{'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':fcurrencyCode,'tcurrencyCode':tcurrencyCode,'history':history,'favpair':favpair,'rates':rates})
