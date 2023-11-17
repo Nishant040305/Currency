666a100cfb031982c59bd11b78c34c02cffd8aee
Mukund Bihari Gupta <142800930+mukund-gupta21@users.noreply.github.com>
2023-11-17 19:52:51 +0530

    Update dict_filters.py

--- Currency_Converter/templatetags/dict_filters.py
+++ Currency_Converter/templatetags/dict_filters.py
@@ -1,1 +1,10 @@
+from django import template
 
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+@register.filter
+def rounding(obj,num):
+    return round(float(obj.fAmount),3)
