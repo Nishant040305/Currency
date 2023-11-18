32222a38de779f21e159affd7273c44382afefaf
Pab256 <101547243+Pab256@users.noreply.github.com>
2023-11-18 23:13:22 +0530

    Update urls.py

--- Currency_Converter/urls.py
+++ Currency_Converter/urls.py
@@ -4,5 +4,5 @@ from Currency_Converter import views
 urlpatterns = [
     #it will direct it to index func of views
     path('',views.index,name = 'home'),
-
+    path('news/',views.news,name = 'news'),
 ]
