d9f155cecef70ebb646191631f3c82b1c49092de
Mukund Bihari Gupta <142800930+mukund-gupta21@users.noreply.github.com>
2023-11-24 21:00:43 +0530

    Update urls.py

--- Currency_Converter/urls.py
+++ Currency_Converter/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from Currency_Converter import views
 urlpatterns = [
     #it will direct it to index func of views
-    path('',views.index,name = 'home'),
-    path('news/',views.news,name = 'news'),
-    path('home/',views.index,name = 'home'),
+    path('',views.index,name = 'home'),#home page
+    path('news/',views.news,name = 'news'),#news page
+    path('home/',views.index,name = 'home'),#home page
 ]
