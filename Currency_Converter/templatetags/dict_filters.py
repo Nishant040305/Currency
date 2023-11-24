e87c0a25cd21abf65c1fde77f4e04dabb851b924
Mukund Bihari Gupta <142800930+mukund-gupta21@users.noreply.github.com>
2023-11-24 21:05:36 +0530

    Update dict_filters.py

--- Currency_Converter/templatetags/dict_filters.py
+++ Currency_Converter/templatetags/dict_filters.py
@@ -1,15 +1,19 @@
 from django import template
+"""function to be used in the template"""
+
 
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
+def get_item(dictionary, key):#to get value in dict using key
     return dictionary.get(key)
+    
 @register.filter
-def rounding(obj,num):
+def rounding(obj,num):#to round of the digits in history 
     return round(float(obj.fAmount),3)
+    
 @register.filter
-def flag(dictionary,value):
+def flag(dictionary,value):# to get the key using value
     for i in dictionary:
         if dictionary[i]== value:
             break
