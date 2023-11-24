2fe6d8ca7504fa30ab3338d2d69fb65906e60fec
Mukund Bihari Gupta <142800930+mukund-gupta21@users.noreply.github.com>
2023-11-24 20:29:00 +0530

    Update views.py

--- Currency_Converter/views.py
+++ Currency_Converter/views.py
@@ -9,22 +9,30 @@ from csv import reader
 from django.contrib.staticfiles.storage import staticfiles_storage
 from .models import favPain
 from .models import History
+
+#proxie of mnnit
 proxies = {
   'http': 'http://edcguest:edcguest@172.31.100.27:3128',
   'https': 'http://edcguest:edcguest@172.31.100.27:3128',
 }
 
 
-#
+# location of currency_name file( include (currency 2 letter code and 3 letter currency code)
 csv_file_path = staticfiles_storage.path('currency_name.csv')
 content = {}
 with open(csv_file_path,'r') as f:
-    a =( reader(f))
+    a =(reader(f))
     for i in a:
       content[f"""/static/flag/{i[0]}.png"""]=i[1]
+#data of csv file
+
+#duration for reload
 duration = 6000
 start = time.time()
+
+#loading data of Api (exchange rate and news)
 try:
+  #for normal
     API_KEY = "45e8907276ac4e1e9a77d3e83a9f19f7"
     url = "https://newsapi.org/v2/everything?q="
     query="Finance"
@@ -44,14 +52,19 @@ except:
     article = news['articles']
     url ="https://api.exchangerate-api.com/v4/latest/USD"
     response = requests.get(url,proxies=proxies).json()
-rates = response[ "rates"]
-cur_ = "USD"
-def exchange(exrates,rates,cur):
+
+rates = response[ "rates"] #for dict to hold exchange rates
+cur_ = "USD" # to hold the present currency of exchange rate table
+
+def exchange(rates,cur):#fuction to get exchange rate for different currency
+  exrates = {}
   rate_cur = rates[cur]
   for i in rates:
     exrates[i]=round(rates[i]/rate_cur,4)
   return exrates
-def index(request):
+
+
+def index(request):#main function
     global content
     global response
     global start
@@ -60,40 +73,45 @@ def index(request):
     global exrates
     global query
     global cur_
-    exchange_cur=request.GET.get("exchange_data")
+
     favpair = favPain.objects.all()
     history = History.objects.all()
-    try:
+    
+    try:#condition of reload
         if((time.time()-start)>duration):
             url ="https://api.exchangerate-api.com/v4/latest/USD"
             response = requests.get(url).json()
             start = time.time()
-            rates = response['rates']
-            exrates=exchange(exrates,rates,cur_)
+            rates = response['rates'] #reload rates
+            exrates=exchange(exrates,rates,cur_)# data for live according to currency
     except:
         pass
 
-    if request.GET.get('fav'):
+    if request.GET.get('fav'):#function to store data in favPain database(favourites)
         tAmount=request.GET.get('bamount')
         fAmount=request.GET.get('oamount')
         field1_data = request.GET.get('fromCode')
         field2_data = request.GET.get('toCode')
-        if(field1_data!='Search'and field2_data!='Search'):
+      
+        if(field1_data!='Search'and field2_data!='Search'):#to check that the value of fromCode and toCode is correct that is selected
     
-            data = favPain.objects.create(fromCode=field1_data, toCode=field2_data)
+            data = favPain.objects.create(fromCode=field1_data, toCode=field2_data)#to store the object into database
             data.save()
+          
         return render(request,'index.html',{"x":cur_,'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'history':history,'favpair':favpair,'rates':rates,'exrates':exrates})
-    elif request.GET.get('exchange'):
-        tAmount=request.GET.get('bamount')
-        fAmount=request.GET.get('oamount')
-        field1_data = request.GET.get('fromCode')
-        field2_data = request.GET.get('toCode')
-        cur=request.GET["currency"]
-        exrates = exchange(exrates,rates,cur)
+    
+    elif request.GET.get('exchange'):#to check exchange rate change button is pressed or not
+        fcurrencyCode=request.GET['fromCode']
+        fAmount=request.GET['oamount']
+        tcurrencyCode=request.GET['toCode']
+        tAmount=request.GET['bamount']
+        cur=request.GET["currency"]#to get the value of its search value
+        exrates = exchange(rates,cur)
         cur_ = cur
         return render(request,'index.html',{"x":cur_,'exrates':exrates,'rates':rates,'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'history':history,'favpair':favpair,'rates':rates})
-    elif request.GET.get('convert'):
-        try:
+    
+    elif request.GET.get('convert'):#to check if exchange rate button is pressed or not for conversion process
+        try:#to save in history
             fcurrencyCode=request.GET['fromCode']
             fAmount=request.GET['oamount']
             tcurrencyCode=request.GET['toCode']
@@ -102,8 +120,7 @@ def index(request):
             data = History.objects.create(fcurrencyCode=fcurrencyCode,fAmount=fAmount,tcurrencyCode=tcurrencyCode,tAmount=tAmount)
             data.save()
 
-
-        except:
+        except:#if something went wrong
             tAmount=0.00
             fcurrencyCode=request.GET['fromCode']
             fAmount=0.00
@@ -111,14 +128,6 @@ def index(request):
 
         return render(request,'index.html',{"x":cur_,'exrates':exrates,'context':content,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':fcurrencyCode,'tcurrencyCode':tcurrencyCode,'history':history,'favpair':favpair,'rates':rates})
 
-    elif request.GET.get('calcwindow'):
-        
-
-        tAmount=request.GET.get('bamount')
-        fAmount=request.GET.get('oamount')
-        field1_data = request.GET.get('fromCode')
-        field2_data = request.GET.get('toCode')
-        return render(request,'index.html',{"x":cur_,'context':content,'exrates':exrates,'tAmount':tAmount,'fAmount':fAmount,'fcurrencyCode':field1_data,'tcurrencyCode':field2_data,'favpair':favpair,'rates':rates,'history':history})
     elif request.GET.get("News"):
         return render(request,'news.html')
     else:
